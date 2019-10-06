49a2ccadf32c9421e89b078a3160dcfaabb93f88
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-10-06 19:21:49 +0100

    remove comments

--- src/lesson-code/AsyncTracker.js
+++ src/lesson-code/AsyncTracker.js
@@ -29,12 +29,11 @@ const loadVariations = merge(loadUp, loadDown);
 const currentLoadCount = loadVariations.pipe(
   scan((currLoads, loadEvent) => {
     const newCount = currLoads + loadEvent;
-    //ensures it doesn't go below zero
     return newCount > 0 ? newCount : 0;
   }, 0),
   startWith(0),
-  distinctUntilChanged(), //in case of multiple zeros,
-  shareReplay(1) //show how it doesn't work - then add the share() - then show why we need to change to shareReplay(1)
+  distinctUntilChanged(),
+  shareReplay(1)
 );
 
 const hideSpinner = currentLoadCount.pipe(filter(count => count === 0));
@@ -101,36 +100,6 @@ function displaySpinner(total, loaded) {
     };
   });
 }
-
-/*
-1. the above - initial show and hide, connect the service as an observable
-2. requirements come in - it shouldn't flash, it needs the delay when appearing and delay before dissapearing
-3. display loading bar as they come, use the max() with repeat() to count loads
-4. konami - hide when sequence activated
-5. tracker operator and trackable promise
-*/
-
-/*
-after writing the loadStarts and loadEnds subject:
-so I know what we've done doesn't seem like a big deal
-but now we are fully in RxJS functional land. At this point, we're done
-with all this mental context of services and how it's going to be used
-in the Ionic app. We can now focus and work with just these two observables:
-one that tells us when a load started, and another one that tells us
-when it ended.
-
-Try to see if I can work backwards - build the larger observable
-as pseudo code and work backwards
-
-say that we can make it to accept observables - but that would
-introduce a range of things we have to cater for - for example, we want this
-to be a passive service, that doesn't generate any other work besides
-showing the bar at the bottom - so because of that we'd only be able to send
-in hot observables, as cold ones generate new subscriptions
-or what if we want to use promises?
-we want to keep it as simple as possible, we'll use an up and down method for now
-we'll then build further abstractions on top of it
-*/
 
 export default {
   taskCompleted,
--- src/lesson-code/EventCombo.js
+++ src/lesson-code/EventCombo.js
@@ -15,7 +15,6 @@ const keyPresses = fromEvent(document, "keypress").pipe(
 export function keyboardCombo(keyCombo) {
   return keyPresses.pipe(
     exhaustMap(key => {
-      //start with a switchMap first, show why it doesn't work, then switch to exhaustMap
       return concat(of(key), keyPresses).pipe(
         takeWhile((key, index) => keyCombo[index] === key),
         filter((_, index) => index === keyCombo.length - 1),
