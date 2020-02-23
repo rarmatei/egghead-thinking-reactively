d4d2efcb88a54c9acd815559efa6acec40b04619
Rares Matei <matei.rar@gmail.com>
2020-02-23 12:31:56 +0000

    end task on disposal

--- src/lesson-code/Extensions.js
+++ src/lesson-code/Extensions.js
@@ -3,19 +3,23 @@ import { newTaskStarted, existingTaskCompleted } from "./TaskProgressService";
 
 export function showLoadingStatus() {
   return source => {
-    return new Observable(observer => {
+    return new Observable(subscriber => {
       newTaskStarted();
-      return source.subscribe({
-        next: val => observer.next(val),
+      const innerSubscription = source.subscribe({
+        next: val => subscriber.next(val),
         error: err => {
           existingTaskCompleted();
-          observer.error(err);
+          subscriber.error(err);
         },
         complete: () => {
           existingTaskCompleted();
-          observer.complete();
+          subscriber.complete();
         }
       });
+      return () => {
+        existingTaskCompleted();
+        innerSubscription.unsubscribe();
+      };
     });
   };
 }
