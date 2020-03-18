9b3f52289207c712ad346bf03d3bb5cd7409e09e
Rares Matei <matei.rar@gmail.com>
2020-03-18 23:03:07 +0000

    lesson2

--- src/lesson-code/TaskProgressService.js
+++ src/lesson-code/TaskProgressService.js
@@ -1,7 +1,29 @@
-import { Observable } from "rxjs";
+import { Observable, merge } from "rxjs";
+import { mapTo, scan } from "rxjs/operators";
+
+/*
+  How do we count?
+    Start from zero
+    When an async task starts, increase the count by 1
+    When a task ends, decrease the count by 1
+*/
 
 const taskStarts = new Observable();
 const taskCompletions = new Observable();
 const showSpinner = new Observable();
+
+const loadUp = taskStarts.pipe(mapTo(1));
+const loadDown = taskCompletions.pipe(mapTo(-1));
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx //
+
+const loadVariations = merge(loadUp, loadDown);
+
+const currentLoadCount = loadVariations.pipe(
+  scan((totalCurrentLoads, changeInLoads) => {
+    const newLoadCount = totalCurrentLoads + changeInLoads;
+    return newLoadCount < 0 ? 0 : newLoadCount;
+  }, 0)
+);
 
 export default {};
